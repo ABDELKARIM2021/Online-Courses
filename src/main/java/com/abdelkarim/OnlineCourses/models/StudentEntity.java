4cd9569e07230b8f8032b52ec71c7e1bba3875cc
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-06-07 23:25:13 +0100

    Create StudentEntity.java

---
+++ src/main/java/com/abdelkarim/OnlineCourses/models/StudentEntity.java
@@ -0,0 +1,48 @@
+package com.abdelkarim.OnlineCourses.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name="student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentEntity implements Serializable{
+	
+	
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	
+	
+	@Column(name="nom")
+	private String nom;
+	
+	
+	
+	@Column(name="prenom")
+	private String prenom;
+	
+	
+	
+	@Column(name="mail")
+	private String mail;
+	
+	
+	
+	@Column(name="password")
+	private String password;
+
+}
