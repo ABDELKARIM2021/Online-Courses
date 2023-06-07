35d36e7843fe0f950d8e6ef64bc0e44e57c6bf1e
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-06-07 21:36:09 +0100

    admin table

---
+++ src/main/java/com/abdelkarim/OnlineCourses/models/AdminEntity.java
@@ -0,0 +1,56 @@
+package com.abdelkarim.OnlineCourses.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name="admin")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminEntity implements Serializable {
+	
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+		@Id
+		@GeneratedValue
+		private Integer id;
+		
+		
+		@Column(name="ref")
+		private String ref;
+		
+		
+		
+		@Column(name="nom")
+		private String nom;
+		
+		
+		
+		@Column(name="prenom")
+		private String prenom;
+		
+		
+		
+		
+		
+		@Column(name="mail")
+		private String mail;
+	
+	
+
+}
